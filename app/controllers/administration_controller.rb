e11915eb86ec0c7e1507e3d76c3bf84f1dc1588a
timmtonga <timmtonga@gmail.com>
2013-09-05 16:38:12 +0200

    worked on acknowledging recieved demographics and ids

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -67,4 +67,84 @@ class AdministrationController < ApplicationController
     redirect_to :controller => 'demographics', :action => "people_drill_down", :title => params[:title], :keys => keys, :data => data
   end
 
+  def acknowledge_received_ids
+
+    @mode = YAML.load_file("#{Rails.root}/config/application.yml")['dde_mode']
+    if @mode == "vh"
+      @options = NationalIdentifier.find(:all,:conditions => ["request_vh_notified = 0"]).map{|x| [x.identifier,x.identifier]}
+    elsif @mode == "gvh"
+      @options = NationalIdentifier.find(:all,:conditions => ["request_gvh_notified = 0"]).map{|x| [x.identifier,x.identifier]}
+    end
+  end
+
+  def acknowledge_received_demographics
+
+
+    @options = NationalIdentifier.find(:all, :conditions => ["post_gvh_notified = 0 AND person_id IS NOT NULL"]).map{|x| ["#{x.person.name}(#{x.identifier})", x.identifier]}
+
+  end
+  def post_demographics_acknowledgement
+
+    unless params[:demographics].blank?
+      settings = YAML.load_file("#{Rails.root}/config/application.yml")["dde_gvh"] rescue {}
+
+      unless settings.empty?
+        username = settings["username"]
+        password = settings["password"]
+        target = settings["target_server"]
+        url = "http://#{username}:#{password}@#{target}/administration/demographics_ack"
+        values = params[:demographics]
+        params = {:demographics => values}
+        post = RestClient.post(url,params )
+
+        NationalIdentifier.update_all({:post_gvh_notified => 1}, ["identifier IN (?)", values])
+      end
+    end
+    redirect_to "/"
+  end
+
+  def post_ids_acknowledgement
+
+    unless params[:ids].blank?
+      mode = params[:mode]
+      if mode == "vh"
+        settings = YAML.load_file("#{Rails.root}/config/application.yml")["dde_vh"] rescue {}
+      else
+        settings = YAML.load_file("#{Rails.root}/config/application.yml")["dde_gvh"] rescue {}
+      end
+
+      unless settings.empty?
+        username = settings["username"]
+        password = settings["password"]
+        target = settings["target_server"]
+
+        url = "http://#{username}:#{password}@#{target}/administration/ids_ack"
+        values = params[:ids]
+        params = {:ids => values, :mode => mode}
+        post = RestClient.post(url,params )
+
+        if mode == 'vh'
+          NationalIdentifier.update_all({:request_vh_notified => 1}, ["identifier IN (?)", values])
+        elsif mode == 'gvh'
+          NationalIdentifier.update_all({:request_gvh_notified => 1}, ["identifier IN (?)", values])
+        end
+      end
+    end
+    redirect_to "/"
+
+  end
+
+  def ids_ack
+
+    if params[:mode] == 'vh'
+      NationalIdentifier.update_all({:request_vh_notified => 1}, ["identifier IN (?)", params[:ids]])
+    elsif params[:mode] == 'gvh'
+      NationalIdentifier.update_all({:request_gvh_notified => 1}, ["identifier IN (?)", params[:ids]])
+    end
+    render :text => "OK"
+  end
+  def demographics_ack
+    NationalIdentifier.update_all({:post_gvh_notified => 1}, ["identifier IN (?)", params[:demographics]])
+    render :text => "OK"
+  end
 end
