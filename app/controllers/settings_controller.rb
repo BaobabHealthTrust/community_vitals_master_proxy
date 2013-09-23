c182c17d363e7e3146a1203ed2d23234307b2750
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2013-09-23 16:01:37 +0200

    Added settings setup page for desktop app

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,7 +1,6 @@
 class SettingsController < ApplicationController
   
-  def site_setup
-  
+  def site_setup  
   end
   
   def save_setup
@@ -10,19 +9,19 @@ class SettingsController < ApplicationController
     if !settings.empty?
       mode = settings["dde_mode"] rescue nil
       
-      if !mode.nil?
+      if !mode.blank?
         
-        settings["dde_#{mode}"]["batch_count"] = params[:batch].to_i if !params[:batch].nil?
+        settings["dde_#{mode}"]["batch_count"] = params[:batch].to_i if !params[:batch].blank?
         
-        settings["dde_#{mode}"]["site_code"] = params[:code].to_s if !params[:code].nil?
+        settings["dde_#{mode}"]["site_code"] = params[:code].to_s if !params[:code].blank?
         
-        settings["dde_#{mode}"]["threshold"] = params[:threshold].to_i if !params[:threshold].nil?
+        settings["dde_#{mode}"]["threshold"] = params[:threshold].to_i if !params[:threshold].blank?
         
-        settings["dde_#{mode}"]["password"] = params[:password].to_s if !params[:password].nil?
+        settings["dde_#{mode}"]["password"] = params[:password].to_s if !params[:password].blank?
         
-        settings["dde_#{mode}"]["username"] = params[:username].to_s if !params[:username].nil?
+        settings["dde_#{mode}"]["username"] = params[:username].to_s if !params[:username].blank?
         
-        settings["dde_#{mode}"]["target_server"] = "#{params[:ip].to_s}:#{params[:port].to_s}" if !params[:ip].nil? && !params[:port].nil?
+        settings["dde_#{mode}"]["target_server"] = "#{params[:ip]}:#{params[:port]}" if !params[:ip].blank? && !params[:port].blank?
         
         file = File.open("#{Rails.root}/config/application.yml", "w")
         
@@ -33,6 +32,32 @@ class SettingsController < ApplicationController
       end
     end
     
+    redirect_to "/"
+  end
+
+  def setup_summary
+    @settings = YAML.load_file("#{Rails.root}/config/application.yml") rescue {}
+    
+    @dde_mode = @settings["dde_mode"] rescue nil
+    
+    @site_code = @settings["dde_#{@dde_mode}"]["site_code"] rescue nil
+    
+    @batch_count = @settings["dde_#{@dde_mode}"]["batch_count"] # rescue nil
+    @target_server = @settings["dde_#{@dde_mode}"]["target_server"] # rescue nil
+    @threshold = @settings["dde_#{@dde_mode}"]["threshold"] # rescue nil
+    @username = @settings["dde_#{@dde_mode}"]["username"] # rescue nil
+    @vh = @settings["dde_#{@dde_mode}"]["vh"] rescue nil
+    @password = @settings["dde_#{@dde_mode}"]["password"] # rescue nil
+    @gvh = @settings["dde_#{@dde_mode}"]["gvh"] # rescue nil
+    @ta = @settings["#{Rails.env}"]["ta"] # rescue nil
+    
+    @available = NationalIdentifier.available_ids # rescue 0
+    
+    @total = 0
+    
+  end
+
+  def done
     redirect_to "/"
   end
 
