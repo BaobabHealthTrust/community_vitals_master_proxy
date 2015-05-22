d4074d5634466b384d3e38c9e5049072b6df33f2
timmtonga <timmtonga@gmail.com>
2015-05-22 11:57:06 +0200

    Added versioning using git taghs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def version
+
+    style = "style='background-color:red;'" unless session[:datetime].blank?
+    "CVR Version: #{APP_VERSION} - <span #{style}>#{(session[:datetime].to_date rescue Date.today).strftime('%A, %d-%b-%Y')}</span>"
+  end
+
+  def welcome_message
+    "Muli bwanji, enter your user information or scan your id card. <span style='font-size:0.6em;float:righti;margin-right: 20px;'>(#{version})</span>"
+  end
+
 end
